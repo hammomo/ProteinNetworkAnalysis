4307f610d07980191cd98c7c8b53833c03ed5afa
soxmomomo <evamore121@hotmail.com>
2019-12-30 12:16:11 +0000

    migrate project to maven env

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # ProteinNetworkAnalysis
 
+For Maven:
 ```shell script
-> cd target/
-> export PATH_TO_FX=path/to/javafx-sdk-13/lib
-> java -jar --module-path $PATH_TO_FX --add-modules javafx.controls,javafx.fxml NetworkAnalysis.jar
+> mvn clean javafx:run
 ```
---
+++ src/main/java/home/HomeController.java
@@ -0,0 +1,182 @@
+package home;
+
+import javafx.beans.value.ChangeListener;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+import logic.Network;
+import logic.Node;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+    @FXML
+    private TextArea summary, infoArea;
+    @FXML
+    private Button upload, add, save, nodeSearch;
+    @FXML
+    private TextField filepath, node1, node2, output, nodeName;
+    @FXML
+    private LineChart lineChart;
+
+    private Stage stage;
+    private Network network;
+    private double averageDegree = 0.0;
+    private int numOfNodes = 0;
+    private int numOfInteractions = 0;
+    private String hubStr = "";
+    private int maxDegree = 0;
+    private int numOfEdges = 0;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        filepath.setText("PPInetwork.txt");
+        setSummary();
+        infoArea.setText("Information Area:\n");
+        infoArea.textProperty().addListener((ChangeListener<Object>) (observable, oldValue, newValue) -> {
+            infoArea.setScrollTop(Double.MAX_VALUE); //this will scroll to the bottom
+        });
+        upload.setOnAction(actionEvent -> {
+            // openFileDialog(); // Cannot work on Mac OS 10.15.1 due to JavaFX internal bug
+            openFile();
+        });
+        nodeSearch.setOnAction(actionEvent -> {
+            String msg = searchDegreeForNode();
+            infoArea.appendText(msg);
+        });
+        add.setOnAction(actionEvent -> {
+            String msg = addInteraction();
+            infoArea.appendText(msg);
+        });
+        save.setOnAction(actionEvent -> {
+            String msg = saveDistribution();
+            infoArea.appendText(msg);
+        });
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    private void setSummary() {
+        if (numOfEdges == 0 && nodeName.getText().equals("")) {
+            summary.setText(String.format("Number of Nodes: %d\nNumber of Interactions: %d\nAverage Degree: %.8f\n" +
+                            "Hub(s): %s\nMax Degree: %d", numOfNodes, numOfInteractions
+                    , averageDegree, hubStr, maxDegree));
+        } else {
+            summary.setText(String.format("Number of Nodes: %d\nNumber of Interactions: %d\nAverage Degree: %.8f\n" +
+                            "Hub(s): %s\nMax Degree: %d\nFind %d edge(s) for the Node %s", numOfNodes, numOfInteractions
+                    , averageDegree, hubStr, maxDegree, numOfEdges, nodeName.getText()));
+        }
+    }
+
+    private void updateSummary() {
+        Pair<Integer, String> p  = network.generateHubsString();
+        hubStr = p.getValue();
+        maxDegree = p.getKey();
+        numOfNodes = network.countOfNodes();
+        numOfInteractions = network.countOfEdges();
+        averageDegree = network.averageDegree();
+    }
+
+    private void openFile() {
+        network = new Network();
+        try {
+            network.createNetworkFromFile(filepath.getText());
+            updateSummary();
+            setSummary();
+            updateLineChart();
+            infoArea.appendText("File " + filepath.getText() + " uploaded successfully!\n");
+        } catch (IOException e) {
+            infoArea.appendText("Cannot find file: " + e.getMessage()
+                    + "\nPlease input valid filename!\n");
+        }
+    }
+
+    private String searchDegreeForNode() {
+        try {
+            checkNetworkExistence();
+        } catch (NullPointerException e) {
+            return e.getMessage();
+        }
+        String name = nodeName.getText();
+        if (name == null || name.equals("")) return "Please enter a node name!\n";
+        int degree = network.degreeOfNode(new Node(name));
+        if (degree == 0) return "Cannot find edges for the Node: " + name + "\n";
+        numOfEdges = degree;
+        setSummary();
+        return String.format("Find %d edge(s) for the Node %s\nSummary Updated!\n", numOfEdges, name);
+    }
+
+    private String addInteraction() {
+        try {
+            checkNetworkExistence();
+        } catch (NullPointerException e) {
+            return e.getMessage();
+        }
+        try {
+            String msg = network.addInteraction(node1.getText(), node2.getText());
+            updateSummary();
+            setSummary();
+            updateLineChart();
+            return msg;
+        } catch (NullPointerException e) {
+            return "Cannot add Null node! Please input both nodes!\n";
+        }
+    }
+
+    private String saveDistribution() {
+        try {
+            checkNetworkExistence();
+        } catch (NullPointerException e) {
+            return e.getMessage();
+        }
+        String filename = output.getText();
+        if (filename == null || filename.equals("")) return "Save failed! Please specify the output filename!\n";
+        network.saveDegreeDistribution(filename);
+        return String.format("Network distribution has been saved to file: %s\n", filename);
+    }
+
+    private void checkNetworkExistence() throws NullPointerException {
+        if (network == null) throw new NullPointerException("Network has not been initialised!\n");
+    }
+
+    private void updateLineChart() {
+        double xUpper = 0.0, yUpper = 0.0;
+        Map<Integer, Integer> dist = network.getDegreeDistribution();
+        for (int i: dist.keySet()) {
+            if (i > xUpper) xUpper = i;
+            if (dist.get(i) > yUpper) yUpper = dist.get(i);
+        }
+        if (lineChart.getData().size() > 0) {
+            lineChart.getData().remove(0);
+        }
+        lineChart.setLayoutX(xUpper);
+        lineChart.setLayoutY(yUpper);
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        dist.forEach((k,v) -> {
+            series.getData().add(new XYChart.Data(k, v));
+        });
+
+        lineChart.getData().add(series);
+    }
+
+    /**
+     * Useless on Mac OS 10.15.1
+     */
+    private void openFileDialog() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open File");
+        fileChooser.showOpenDialog(stage);
+    }
+}
